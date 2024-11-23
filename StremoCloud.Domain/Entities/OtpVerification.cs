406a6f534336babf05c0009192f7c1f6a99b8ad4
Rankshow <uwaomaobinna20@gmail.com>
2024-11-23 16:39:46 +0100

    Modified otp model

--- StremoCloud.Domain/Entities/OtpVerification.cs
+++ StremoCloud.Domain/Entities/OtpVerification.cs
@@ -4,9 +4,8 @@ namespace StremoCloud.Domain.Entities;
 
 public partial class OtpVerification : Entity
 {
-    //This is a comment
-    //Still a test.
     public string PhoneNumber { get; set; } = string.Empty;
     public string Otp { get; set; } = string.Empty;
-    public DateTime ExpirationTime {  get; set; }
+    public DateTime ExpirationTime { get; set; }
+    public DateTime ExpirationDate { get; set; } = DateTime.Now;
 }
