77f716a1362a8f3fc75b563937207a82c6324ea0
Rankshow <uwaomaobinna20@gmail.com>
2024-11-23 16:31:45 +0100

    added dashboard model

--- StremoCloud.Domain/Entities/Dashboard.cs
+++ StremoCloud.Domain/Entities/Dashboard.cs
@@ -1,7 +1,35 @@
-﻿namespace StremoCloud.Domain.Entities
-{
-    public class Dashboard
-    {
+﻿using System.Transactions;
 
-    }
+namespace StremoCloud.Domain.Entities;
+
+public class Dashboard
+{
+    public int TotalOrders { get; set; }
+    public decimal TotalRevenue { get; set; }
+    public List<TransactionStatus> TransactionStatuses { get; set; }
+    public List<RevenueRecord> RevenueRecords { get; set; }
+    public FilterOptionsEnum Filter { get; set; } = FilterOptionsEnum.AllTime;
+}
+
+public class TransactionStatus
+{
+    public string Status { get; set; } // e.g., "Successful", "Failed"
+    public int Percentage { get; set; }
+}
+
+public class RevenueRecord
+{
+    public DateTime Date { get; set; }
+    public decimal RevenueAmount { get; set; }
+}
+
+public enum FilterOptionsEnum
+{
+    AllTime,
+    LastYear,
+    ThisYear,
+    LastMonth,
+    ThisMonth,
+    LastWeek,
+    ThisWeek
 }
