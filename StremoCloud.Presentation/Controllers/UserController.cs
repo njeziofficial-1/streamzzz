bbe72f9a9046c66993f21eac710da2d89753dde8
njeziofficial-1 <john.njezi@toyingroupsystems.com>
2024-11-23 17:24:54 +0100

    File scoped

--- StremoCloud.Presentation/Controllers/UserController.cs
+++ StremoCloud.Presentation/Controllers/UserController.cs
@@ -3,31 +3,30 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StremoCloud.Application.Features.Command.Create;
 
-namespace StremoCloud.Presentation.Controllers
+namespace StremoCloud.Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IMediator mediator) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController(IMediator mediator) : ControllerBase
+    [HttpPost("register")]
+    public async Task<IActionResult> SignUpAsync(SignUpCommand command)
     {
-        [HttpPost("register")]
-        public async Task<IActionResult> SignUpAsync(SignUpCommand command)
-        {
-            var response = await mediator.Send(command);
-            return Ok(response);
-        }
+        var response = await mediator.Send(command);
+        return Ok(response);
+    }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> SignInAsync(SignInCommand command)
-        {
-            var response = await mediator.Send(command);
-            return Ok(response);
-        }
+    [HttpPost("login")]
+    public async Task<IActionResult> SignInAsync(SignInCommand command)
+    {
+        var response = await mediator.Send(command);
+        return Ok(response);
+    }
 
-        [HttpPost("forgot-password")]
-        public async Task<IActionResult> ForgotPasswordAsync(ForgotPasswordCommand command)
-        {
-            var response = await mediator.Send(command);
-            return Ok(response);
-        }
+    [HttpPost("forgot-password")]
+    public async Task<IActionResult> ForgotPasswordAsync(ForgotPasswordCommand command)
+    {
+        var response = await mediator.Send(command);
+        return Ok(response);
     }
 }
