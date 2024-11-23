409a3b7ede3cc286d2b1128ed5d2ebdf62e2fa24
njeziofficial-1 <john.njezi@toyingroupsystems.com>
2024-11-23 16:28:08 +0100

    Testing

--- StremoCloud.Domain/Entities/OtpVerification.cs
+++ StremoCloud.Domain/Entities/OtpVerification.cs
@@ -5,6 +5,7 @@ namespace StremoCloud.Domain.Entities;
 public partial class OtpVerification : Entity
 {
     //This is a comment
+    //Still a test.
     public string PhoneNumber { get; set; } = string.Empty;
     public string Otp { get; set; } = string.Empty;
     public DateTime ExpirationTime {  get; set; }
