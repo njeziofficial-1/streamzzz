cc83ee4123781ba6ec461eeb763e9eab0b9f3a7f
njeziofficial-1 <john.njezi@toyingroupsystems.com>
2024-11-29 00:06:22 +0100

    Added Get Users

--- StremoCloud.Application/Extensions/ServiceExtensions.cs
+++ StremoCloud.Application/Extensions/ServiceExtensions.cs
@@ -9,23 +9,30 @@ using StremoCloud.Shared.Helpers;
 using StremoCloud.Application.Features.Command.Create;
 using StremoCloud.Domain.Interface;
 using StremoCloud.Infrastructure.Data.UnitOfWork;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 namespace StremoCloud.Application.Extensions;
 
 public static class ServiceExtensions
 {
-    public static void AddApplicationLayer(this IServiceCollection services)
+    public static void AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(x => x.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-        services.AddScoped<IDataContext, DataContext>();
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         services.AddScoped<ISecurityHelper, SecurityHelper>();
         services.AddScoped<IEmailService, EmailService>();
         services.AddScoped<ITokenHelper, TokenHelper>();
-        services.AddScoped<IStremoUnitOfWork, StremoUnitOfWork>();
+        services.AddScoped<IDataContext>(x =>
+        {
+            var client = new MongoClient(configuration["MongoDbOptions:ConnectionString"]);
+            var database = client.GetDatabase(configuration["MongoDbOptions:DatabaseName"]);
+            return new DataContext(configuration, database);
+        });
 
+        services.AddScoped<IStremoUnitOfWork, StremoUnitOfWork>();
         services.AddValidatorsFromAssemblyContaining<ValidateOtpCommandValidator>();
         services.AddScoped<IOtpService, OtpService>();
     }
