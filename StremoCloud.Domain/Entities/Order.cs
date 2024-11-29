87e21df7566af71511c7bf895dc7a681f24e0939
Rankshow <uwaomaobinna20@gmail.com>
2024-11-29 21:41:23 +0100

    added field in the order table

--- StremoCloud.Domain/Entities/Order.cs
+++ StremoCloud.Domain/Entities/Order.cs
@@ -1,12 +1,14 @@
-﻿using StremoCloud.Domain.Enum;
+﻿using StremoCloud.Domain.Common;
+using StremoCloud.Domain.Enum;
 
 namespace StremoCloud.Domain.Entities;
 
-public class OrderDetail
+public class Order : Entity
 {
     public string CustomerName { get; set; }
     public string Address { get; set; }
     public OrderStatusEnum Status { get; set; }
     public bool IsIncomingCall { get; set; }
-    public string ActionButtonText { get; set; } = "View Order";
+    public OrderDecisionEnum Action { get; set; }
+
 }
--- StremoCloud.Domain/Enum/OrderStatusEnum.cs
+++ StremoCloud.Domain/Enum/OrderStatusEnum.cs
@@ -5,3 +5,8 @@ public enum OrderStatusEnum
     Pending = 1,
     Complete,
 }
+public enum OrderDecisionEnum
+{
+    Approved = 1,
+    Decline,
+}
